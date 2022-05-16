915d99f818d75851b1e27fb3f07c3f0850941a88
Lily Kirsche <lkirsche01@gmail.com>
2022-05-15 23:53:52 -0400

    initial file setup

---
+++ src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import Navigation from "./Navigation";
+
+function Header() {
+    return (
+        <div>
+          <Navigation />  
+        </div>
+    );
+}
+
+export default Header;
---
+++ src/components/Project/index.js
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+
+function Project() {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default Project;
