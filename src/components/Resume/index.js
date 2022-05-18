fdeac2cbd183c2598d39e7377274d613a00a80c7
Lily Kirsche <lkirsche01@gmail.com>
2022-05-17 23:15:36 -0400

    style tweaks

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -2,9 +2,30 @@ import React from "react";
 
 function Resume() {
     return (
-        <div>
-            
-        </div>
+        <section>
+            <p> Check out my <a href="#"><span>resume</span></a>! (WIP) </p>
+
+            <h4>Front-end Skills</h4>
+            <ul>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>jQuery</li>
+                <li>Responsive Design</li>
+                <li>Bootstrap</li>
+                <li>Bulma</li>
+                <li>React</li>
+            </ul>
+
+            <h4>Back-end Skills</h4>
+            <ul>
+                <li>Node.js</li>
+                <li>Express.js</li>
+                <li>MySQL + Sequelize</li>
+                <li>MongoDB + Mongoose</li>
+                <li>RESTful APIs</li>
+                <li>GraphQL</li>
+            </ul>
+        </section>
     );
 }
 
